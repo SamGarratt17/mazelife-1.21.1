9db2dd6ae7460fec1a0957e189c52d21b8d552d5
Sam <samgarratt17@gmail.com>
2024-11-21 22:27:24 +0000

    dimension stuff

--- src/main/java/net/cub/mazelife/MazeLife.java
+++ src/main/java/net/cub/mazelife/MazeLife.java
@@ -2,6 +2,10 @@ package net.cub.mazelife;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,5 +18,12 @@ public class MazeLife implements ModInitializer {
 	public void onInitialize() {
 
 		LOGGER.info("Hello Fabric world!");
+
+		CustomPortalBuilder.beginPortal()
+				.frameBlock(Blocks.STONE_BRICKS)
+				.lightWithItem(Items.DEBUG_STICK)
+				.destDimID(Identifier.of(MazeLife.MOD_ID, "liminal"))
+				.tintColor(0, 66, 37)
+				.registerPortal();
 	}
 }
--- src/main/java/net/cub/mazelife/MazeLifeDataGenerator.java
+++ src/main/java/net/cub/mazelife/MazeLifeDataGenerator.java
@@ -2,10 +2,21 @@ package net.cub.mazelife;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.cub.mazelife.datagen.*;
+import net.cub.mazelife.world.dimension.ModDimensions;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class MazeLifeDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModWorldGenerator::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.DIMENSION_TYPE, ModDimensions::bootstrapType);
 	}
 }
