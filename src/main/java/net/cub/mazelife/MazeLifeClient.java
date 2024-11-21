3cfb3e4b0a856f001df373be859b2aefd33ff0ab
Sam <samgarratt17@gmail.com>
2024-11-21 19:33:44 +0000

    Initial

---
+++ src/main/java/net/cub/mazelife/MazeLifeClient.java
@@ -0,0 +1,10 @@
+package net.cub.mazelife;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MazeLifeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/cub/mazelife/MazeLifeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cub.mazelife;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MazeLifeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
